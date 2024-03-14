1a3b4d04e4e40ae89ff6c2c6ae157371a462a41c
Shahzaibrt <153753326+Shahzaibrt@users.noreply.github.com>
2024-03-14 09:38:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("hello my name is shahzaib")
+
+let num1 =prompt ("Enter your first number")
+let num2 =prompt ("Enter your second number")
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let sum = num1 + num2
+let minus = num1 - num2
+let multiply = num1 * num2
+let divide = num1 / num2
+
+alert("Addition=" + sum)
+alert("Subtraction=" + minus)
+alert("Multiplication=" + multiply)
+alert("Division=" + divide)
